ec418ff6922871dcaee9f881887f22feb5119f86
Laun4er <homecdenis5@gmail.com>
2023-12-23 23:30:57 +0500

    Сделан другой дизайн выключателя

--- TheRestLauncher/Pages/Settings.xaml.cs
+++ TheRestLauncher/Pages/Settings.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace TheRestLauncher.Pages
 {
@@ -28,11 +30,17 @@ namespace TheRestLauncher.Pages
 
             if (rline[line - 1] == A1)
             {
-                AllowUpdate.Content = "Обновления разрешены";
+                AllowUpdate.Content = new Image
+                {
+                    Source = new BitmapImage(new Uri("/Pages/Resources/Buttons/Allow.png", UriKind.Relative))
+                };
             }
             else
             {
-                AllowUpdate.Content = "Обновления запрещены";
+                AllowUpdate.Content = new Image
+                {
+                    Source = new BitmapImage(new Uri("/Pages/Resources/Buttons/Deni.png", UriKind.Relative))
+                };
             }
         }
         private void AllowUpdate_Click(object sender, System.Windows.RoutedEventArgs e)
@@ -46,12 +54,18 @@ namespace TheRestLauncher.Pages
             if (line[lineToReplace - 1] == text1)
             {
                 line[lineToReplace - 1] = text2;
-                AllowUpdate.Content = "Обновления запрещены";
+                AllowUpdate.Content = new Image
+                {
+                    Source = new BitmapImage(new Uri("/Pages/Resources/Buttons/Allow.png", UriKind.Relative))
+                };
             }
             else
             {
                 line[lineToReplace - 1] = text1;
-                AllowUpdate.Content = "Обновления разрешены";
+                AllowUpdate.Content = new Image
+                {
+                    Source = new BitmapImage(new Uri("/Pages/Resources/Buttons/Deni.png", UriKind.Relative))
+                };
             }
             File.WriteAllLines(arg, line);
         }
