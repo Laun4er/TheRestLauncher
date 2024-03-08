71f39505ce96d208930f9059c2e91a70ce42633b
Laun4er <Laun4er@icloud.com>
2024-03-08 20:15:18 +0500

    Множество изменений

--- TheRestLauncher/Pages/News.xaml.cs
+++ TheRestLauncher/Pages/News.xaml.cs
@@ -1,41 +1,118 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Image = System.Windows.Controls.Image;
 
 namespace TheRestLauncher.Pages
 {
 
     public partial class News : Page
     {
+        // Ваш токен бота Discord
+        private const string BotToken = "OTg0MTQ5ODY2MzE1OTE5NDEz.GEwqNx.ByLG9SNNrTULHblxYty8B5FOc0QqWeNIUb-ww8";
+
+        // ID канала, из которого вы хотите получать последнее сообщение
+        private const ulong ChannelId = 1185924268702314536;
+
+        
+
+        // Клиент Discord.NET для подключения к Discord API
+        private DiscordSocketClient _client;
         public News()
         {
             InitializeComponent();
-            Whats_New();
-        }
 
-       public void Whats_New()
+        }
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            string text = "C:\\TheRest\\Launcher\\Whats_New";
-            string Read = File.ReadAllText(text);
+            // Создаем новый клиент Discord.NET с настройками
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                // Логгируем все события
+                LogLevel = LogSeverity.Info
+            });
 
-            WN.Text = Read;
-            
+            // Подписываемся на событие готовности клиента
+            _client.Ready += Client_Ready;
+
+            // Подписываемся на событие получения нового сообщения
+            _client.MessageReceived += Client_MessageReceived;
+
+            // Подключаемся к Discord API с токеном бота
+            await _client.LoginAsync(TokenType.Bot, BotToken);
+            await _client.StartAsync();
+        }
+        private async Task Client_Ready()
+        {
+            // Выводим в консоль информацию о подключении
+            Console.WriteLine($"Logged in as {_client.CurrentUser.Username}#{_client.CurrentUser.Discriminator}");
         }
 
+        private async Task Client_MessageReceived(SocketMessage message)
+        {
+            // Проверяем, что сообщение пришло из нужного канала и не является системным
+            if (message.Channel.Id == ChannelId && !message.Author.IsBot && !message.Author.IsWebhook)
+            {
+                // Вызываем метод для отображения сообщения в окне приложения
+                // в основном потоке с помощью диспетчера
+                Dispatcher.Invoke(() => DisplayMessage(message));
+            }
+        }
 
+        private void DisplayMessage(SocketMessage message)
+        {
+            // Очищаем содержимое StackPanel, где будем отображать сообщение
+            spMessage.Children.Clear();
+
+            // Создаем новый TextBlock для имени пользователя
+            var tbUsername = new TextBlock
+            {
+                Text = message.Author.Username,
+                FontWeight = FontWeights.Bold,
+                Margin = new Thickness(5)
+            };
+
+            // Добавляем TextBlock в StackPanel
+            spMessage.Children.Add(tbUsername);
+
+            // Создаем новый TextBlock для текста сообщения
+            var tbContent = new TextBlock
+            {
+                Text = message.Content,
+                TextWrapping = TextWrapping.Wrap,
+                Margin = new Thickness(5)
+            };
+
+            // Добавляем TextBlock в StackPanel
+            spMessage.Children.Add(tbContent);
+
+            // Если в сообщении есть вложения, то перебираем их
+            if (message.Attachments.Count > 0)
+            {
+                foreach (var attachment in message.Attachments)
+                {
+                    // Если вложение является изображением, то создаем новый Image для него
+                    if (attachment.Width.HasValue && attachment.Height.HasValue)
+                    {
+                        var imgAttachment = new Image
+                        {
+                            Source = new BitmapImage(new Uri(attachment.Url)),
+                            MaxWidth = 400,
+                            MaxHeight = 400,
+                            Margin = new Thickness(5)
+                        };
+
+                        // Добавляем Image в StackPanel
+                        spMessage.Children.Add(imgAttachment);
+                    }
+                }
+            }
+        }
     }
 }
+    
+
--- TheRestLauncher/Pages/Settings.xaml.cs
+++ TheRestLauncher/Pages/Settings.xaml.cs
@@ -8,63 +8,10 @@ namespace TheRestLauncher.Pages
 
     public partial class Settings : Page
     {
-        private const string arguments = "C:\\TheRest\\Launcher\\TheRest.arg";
-
         public Settings()
         {
             InitializeComponent();
-            CheckUpdate();
-        }
-
-        public void CheckUpdate()
-        {
-            string arg = arguments;
-            int line = 10;
-            string A1 = "AllowUpdate = true";
-            string[] rline = File.ReadAllLines(arg);
-
-            if (rline[line - 1] == A1)
-            {
-                AllowUpdate.Content = new Image
-                {
-                    Source = new BitmapImage(new Uri("/Pages/Resources/Buttons/Allow.png", UriKind.Relative))
-                };
-            }
-            else
-            {
-                AllowUpdate.Content = new Image
-                {
-                    Source = new BitmapImage(new Uri("/Pages/Resources/Buttons/Deni.png", UriKind.Relative))
-                };
-            }
-        }
-        private void AllowUpdate_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            string arg = arguments;
-            int lineToReplace = 10;
-            string text1 = "AllowUpdate = true";
-            string text2 = "AllowUpdate = false";
-
-            string[] line = File.ReadAllLines(arg);
-            if (line[lineToReplace - 1] == text1)
-            {
-                line[lineToReplace - 1] = text2;
-                AllowUpdate.Content = new Image
-                {
-                    Source = new BitmapImage(new Uri("/Pages/Resources/Buttons/Deni.png", UriKind.Relative))
-                };
-            }
-            else
-            {
-                line[lineToReplace - 1] = text1;
-                AllowUpdate.Content = new Image
-                {
-                    Source = new BitmapImage(new Uri("/Pages/Resources/Buttons/Allow.png", UriKind.Relative))
-                };
-            }
-            File.WriteAllLines(arg, line);
+            
         }
     }
-    
-
 }
